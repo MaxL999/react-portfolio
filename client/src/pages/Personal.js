e569f67e0b1b4dc16e0d8ba1ea9d8aa30ee5967a
Max Lewis <max.a.lewis98@gmail.com>
2020-05-20 16:55:38 -0700

    more personal page, trying to find good responsive setup for personal info, resume is going to display roughly the same info but in a more formal possibly downloadable to pdf form

--- client/src/pages/Personal.js
+++ client/src/pages/Personal.js
@@ -6,9 +6,9 @@ import Navbar from '../components/Navbar';
 function Personal() {
   return <>
     <Navbar />
-    <a href='#section1'> Go to section 1 </a>
-    <a href='#section2'> Go to section 2 </a>
-    <div className="card m-3">
+
+
+    {/* <div className="card m-3">
       <div className="card-header">
         <h2>Maximilian A. Lewis</h2>
       </div>
@@ -20,19 +20,26 @@ function Personal() {
         <h5 className="cart-title">Born and raised</h5>
         <p className="card-text"> Originally born in Indiana, my family moved to Arizona when I was a toddler.</p>
       </div>
-    </div>
-    
-    <div className="min-height">
-      <ScrollableAnchor id={'section1'} >
-        <div> Hello World </div>
-      </ScrollableAnchor>
-    </div>
-    <div className="min-height">
-    <ScrollableAnchor id={'section2'}>
-      <div> How are you world? </div>
-    </ScrollableAnchor>
-    </div>
+    </div> */}
 
+    {/* d-none d-md-block means "this isnt displayed unless size md or larger, then its a block" */}
+    <nav className="d-none d-md-block">
+      <ul>
+        <li><a href='#section1'> Go to section 1 </a></li>
+        <li><a href='#section2'> Go to section 2 </a></li>
+      </ul>
+    </nav>
+
+    <ScrollableAnchor id={'section1'} >
+      <div className="min-height">
+        <div> Hello World </div>
+      </div>
+    </ScrollableAnchor>
+    <ScrollableAnchor id={'section2'}>
+      <div className="min-height">
+        <div> How are you world? </div>
+      </div>
+    </ScrollableAnchor>
 
   </>
 }
