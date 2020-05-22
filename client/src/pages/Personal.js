aa84debeff96985ece636a32c3e2083eaf822c14
Max Lewis <max.a.lewis98@gmail.com>
2020-05-21 18:59:14 -0700

    personal page about and interests section

--- client/src/pages/Personal.js
+++ client/src/pages/Personal.js
@@ -9,14 +9,6 @@ import placeholder from '../img/placeholder';
 function Personal() {
   return <>
     <Navbar />
-
-    {/* about
-    experience
-    education
-    skills
-    Interests
-    certifications */}
-
     {/* <div className="card m-3">
       <div className="card-header">
         <h2>Maximilian A. Lewis</h2>
@@ -31,36 +23,56 @@ function Personal() {
       </div>
     </div> */}
 
-    {/* d-none d-md-block means "this isnt displayed unless size md or larger, then its a block" */}
+    {/*
+    experience/ previous jobs
+    education
+    skills
+    certifications */}
 
     <div className="container-fluid">
+
       <div className="row">
-        <div className="col-lg-3 border d-flex justify-content-center">
-          <div className="sidebar border mt-5 p-2">
+        <div className="col-lg-3 border p-0 d-flex justify-content-center">
+          <section className="sidebar border my-5 p-2">
             <img src={placeholder} alt="" width="200" height="200" />
 
             <ul className="border">
-              <li><a href='#section1'> Go to section 1 </a></li>
-              <li><a href='#section2'> Go to section 2 </a></li>
-              <li><a href='#section3'> Go to section 3 </a></li>
+              <li><a href='#About'>About</a></li>
+              <li><a href='#Interests'>Interests</a></li>
+              {/* <li><a href='#section3'> Go to section 3 </a></li>
               <li><a href='#section4'> Go to section 4 </a></li>
-              <li><a href='#section5'> Go to section 5 </a></li>
-              <li><a href='#section6'> Go to section 6 </a></li>
+              <li><a href='#section5'> Go to section 5 </a></li> */}
             </ul>
-          </div>
+          </section>
         </div>
-        <div className="col-lg-9 border">
-          <ScrollableAnchor id={'section1'} >
-            <div className="min-height">
-              <div> Hello World </div>
+        <div className="col-lg-9 border p-0 m-0">
+          <ScrollableAnchor id={'About'} >
+            <div className="min-height mb-1 d-flex align-items-center">
+              <div className="p-5">
+                <h2>Maximilian A. Lewis</h2>
+                <p>3753 W. Wayne Ln. 480-625-5534 Max.a.lewis98@gmail.com</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+              </div>
             </div>
           </ScrollableAnchor>
-          <ScrollableAnchor id={'section2'}>
-            <div className="min-height">
-              <div> How are you world? </div>
+          <ScrollableAnchor id={'Interests'}>
+            <div className="min-height mb-1 d-flex align-items-center">
+              <div className="p-5">
+                <div className="p-4">
+                  <h2>Born and raised</h2>
+                  <p>Originally born in Indiana, my family moved to Arizona when I was a toddler.</p>
+                </div>
+                <div className="p-4">
+                  <h2>Personal Interests</h2>
+                  <p>I enjoy skeet shooting and skiing, video games suffice when the weather isnt agreable</p>
+                </div>
+              </div>
             </div>
           </ScrollableAnchor>
-          <ScrollableAnchor id={'section3'}>
+          {/* <ScrollableAnchor id={'section3'}>
             <div className="min-height">
               <div> How are you world? </div>
             </div>
@@ -74,18 +86,12 @@ function Personal() {
             <div className="min-height">
               <div> How are you world? </div>
             </div>
-          </ScrollableAnchor>
-          <ScrollableAnchor id={'section6'}>
-            <div className="min-height">
-              <div> How are you world? </div>
-            </div>
-          </ScrollableAnchor>
+          </ScrollableAnchor> */}
         </div>
       </div>
     </div>
 
     <Footer />
-
   </>
 }
 
