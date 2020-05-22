ac7c2db3b2e99587202f369bf3e624e2c63b8a70
Max Lewis <max.a.lewis98@gmail.com>
2020-05-21 19:15:46 -0700

    hangman node link added

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -18,12 +18,12 @@ class Portfolio extends Component {
             }, {
                 name: "Hangman!",
                 lang: ["Javascript"],
-                desc: "A classic hangman game made with constructors that can be accessed on a webpage or directly thorugh a terminal using Node.",
+                desc: "A classic hangman game made with constructors that can be accessed on a webpage or directly thorugh a terminal using Node.js",
                 git: "https://github.com/MaxL999/3-WordGuess",
                 link1: "https://maxl999.github.io/3-WordGuess/",
                 link1Name: "Webpage",
-                link2: false,
-                link2Name: false
+                link2: "https://github.com/MaxL999/9-hangman.node",
+                link2Name: "Terminal Proof"
             }, {
                 name: "Minesweeper",
                 lang: ["Javascript"],
