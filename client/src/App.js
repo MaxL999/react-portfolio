3469cc6d7457153a05277a2a48bdae054fa50635
Max Lewis <max.a.lewis98@gmail.com>
2020-05-22 21:45:22 -0700

    intro button animation work

--- client/src/App.js
+++ client/src/App.js
@@ -13,15 +13,6 @@ import Portfolio from "./pages/Portfolio";
 import NoMatch from "./components/NoMatch";
 
 function App() {
-
-  // const location = useLocation()
-  // console.log(location)
-  // const transitions = useTransition(location, location => location.pathname, {
-  //   from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
-  //   enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-  //   leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
-  // })
-
   return (
     <Switch>
       <Route exact path="/" render={HomePage} />
@@ -32,18 +23,6 @@ function App() {
       <Route component={NoMatch} />
     </Switch>
   );
-  // return transitions.map(({ item: location, props, key }) => (
-  //   <animated.div key={key} style={props}>
-  //     <Switch location={location}>
-  //       <Route exact path="/" render={HomePage} />
-  //       <Route path="/Resume" render={Resume} />
-  //       <Route path="/Personal" render={Personal} />
-  //       <Route path="/Portfolio" render={() => <Portfolio />} />
-  //       {/* 404 redirect */}
-  //       <Route component={NoMatch} />
-  //     </Switch>
-  //   </animated.div>
-  // ))
 }
 
 export default App;
--- client/src/components/IntroBtn.js
+++ client/src/components/IntroBtn.js
@@ -1,44 +1,40 @@
-// import React, { Component } from "react";
-// // import { render } from "react-dom";
-// // import { Circle } from "./circle";
-// // import "./Documents.css";
-
-// class IntroBtn extends Component {
-//     constructor() {
-//         super(...arguments);
-//         this.state = { hover: false };
-//     }
-
-//     render() {
-//         const { hover } = this.state;
-
-//         return (
-//             <div
-//                 className="Document"
-//                 onMouseEnter={_ => this.setState({ hover: true })}
-//                 onMouseLeave={_ => this.setState({ hover: false })}
-//             >
-//                 <div className="meta">Article</div>
-//                 <div className="title">The title</div>
-//                 <Circle
-//                     animation={{ in: hover }}
-//                     color="pink"
-//                     icon={{ src: "", size: 15 }}
-//                     size={40}
-//                 />
-//             </div>
-//         );
-//     }
-// }
-
-// export default IntroBtn
-
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useSpring, animated } from 'react-spring';
 
-function IntroBtn() {
-    const props = useSpring({ opacity: 1, from: { opacity: 0 } })
-    return <animated.div style={props}>I will fade in</animated.div>
+function IntroBtn(props) {
+
+    const items = [
+        { name: "Resume", link: "/Resume" },
+        { name: "Personal", link: "/Personal" },
+        { name: "Portfolio", link: "/Portfolio" }
+    ]
+    const config = { mass: 5, tension: 2000, friction: 200 }
+
+    const spring = useSpring({ opacity: 1, from: { opacity: 0 } })
+
+    const [toggle, set] = useState(true)
+    const trail = useTrail(items.length, {
+        config,
+        opacity: toggle ? 1 : 0,
+        x: toggle ? 0 : 20,
+        height: toggle ? 80 : 0,
+        from: { opacity: 0, x: 20, height: 0 },
+    })
+
+    return <>
+        {/* <animated.div style={spring}>
+            <Link className="nav-link btn" to={props.link}>{props.name}</Link>
+        </animated.div> */}
+        {trail.map(({ x, height, ...rest }, index) => (
+            <animated.div
+                key={items[index]}
+                className=""
+                style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+                <animated.div style={{ height }}>{items[index]}</animated.div>
+            </animated.div>
+        ))}
+    </>
 }
 
 export default IntroBtn
