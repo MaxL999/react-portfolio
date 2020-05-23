63a4fd8f87957640e78d4e49da5c89aef39c1905
Max Lewis <max.a.lewis98@gmail.com>
2020-05-22 21:24:35 -0700

    cant get router page changes to work, useLocation is undefined, google isnt helpful

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch, } from "react-router-dom";
+
+// import { useLocation } from 'react-router'
+// import { useTransition, animated } from 'react-spring'
+
 import "./style.css";
 
 import HomePage from "./pages/HomePage";
@@ -8,22 +12,38 @@ import Personal from "./pages/Personal";
 import Portfolio from "./pages/Portfolio";
 import NoMatch from "./components/NoMatch";
 
-class App extends Component {
+function App() {
 
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" render={HomePage} />
-          <Route path="/Resume" render={Resume} />
-          <Route path="/Personal" render={Personal} />
-          <Route path="/Portfolio" render={() => <Portfolio />} />
-          {/* 404 redirect */}
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
-    );
-  }
+  // const location = useLocation()
+  // console.log(location)
+  // const transitions = useTransition(location, location => location.pathname, {
+  //   from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+  //   enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+  //   leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  // })
+
+  return (
+    <Switch>
+      <Route exact path="/" render={HomePage} />
+      <Route path="/Resume" render={Resume} />
+      <Route path="/Personal" render={Personal} />
+      <Route path="/Portfolio" render={() => <Portfolio />} />
+      {/* 404 redirect */}
+      <Route component={NoMatch} />
+    </Switch>
+  );
+  // return transitions.map(({ item: location, props, key }) => (
+  //   <animated.div key={key} style={props}>
+  //     <Switch location={location}>
+  //       <Route exact path="/" render={HomePage} />
+  //       <Route path="/Resume" render={Resume} />
+  //       <Route path="/Personal" render={Personal} />
+  //       <Route path="/Portfolio" render={() => <Portfolio />} />
+  //       {/* 404 redirect */}
+  //       <Route component={NoMatch} />
+  //     </Switch>
+  //   </animated.div>
+  // ))
 }
 
 export default App;
