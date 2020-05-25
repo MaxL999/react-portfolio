1020df13d2d6973a42a33da1b4415c67b232f446
Max Lewis <max.a.lewis98@gmail.com>
2020-05-24 22:00:10 -0700

    rootElement object inside index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router} from "react-router-dom"
 import App from "./App";
 
+const rootElement = document.getElementById("root");
+
 ReactDOM.render(
     <Router>
         <App />
     </Router>
-    , document.getElementById("root")
+    , rootElement
 );
