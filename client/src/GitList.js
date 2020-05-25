6069f93e31e53c18187f565dbb0197df34c44df2
Max Lewis <max.a.lewis98@gmail.com>
2020-05-24 17:42:51 -0700

    added react clicker pairs game into portfolio, moved portfolio data into seperate file

---
+++ client/src/GitList.js
@@ -0,0 +1,61 @@
+const portfolio = [
+    {
+        name: "Rock, Paper, Scissors",
+        lang: ["Javascript", "Firebase"],
+        desc: "Multiplayer is enabled through use of google firebase",
+        git: "https://github.com/MaxL999/07-RPS-Multiplayer",
+        link1: "https://maxl999.github.io/07-RPS-Multiplayer/singleplayer.html",
+        link1Name: "Singleplayer",
+        link2: "https://maxl999.github.io/07-RPS-Multiplayer/",
+        link2Name: "Multiplayer"
+    }, {
+        name: "Hangman!",
+        lang: ["Javascript"],
+        desc: "A classic hangman game made with constructors that can be accessed on a webpage or directly thorugh a terminal using Node.js",
+        git: "https://github.com/MaxL999/3-WordGuess",
+        link1: "https://maxl999.github.io/3-WordGuess/",
+        link1Name: "Webpage",
+        link2: "https://github.com/MaxL999/9-hangman.node",
+        link2Name: "Terminal Proof"
+    }, {
+        name: "Minesweeper",
+        lang: ["Javascript"],
+        desc: "The classic minesweeper game",
+        git: "https://github.com/MaxL999/minesweeper",
+        link1: "https://maxl999.github.io/minesweeper/",
+        link1Name: "Minesweeper",
+        link2: false,
+        link2Name: false
+    }, {
+        name: "Burger-Breakin",
+        lang: ["MYSQL", "Handlebars", "Heroku"],
+        desc: "A simple app where you can create and parse data, deployed unto the cloud with Heroku",
+        git: "https://github.com/MaxL999/12-burgerHandlebars",
+        link1: "https://burger-handlebars999.herokuapp.com/",
+        link1Name: "Heroku",
+        link2: false,
+        link2Name: false
+    }, {
+        name: "Match Pairs",
+        lang: ["React-Spring", "Heroku"],
+        desc: "Match pairs of tarot cards together, how good is your memory?",
+        git: "",
+        link1: "https://react-clickergame.herokuapp.com/",
+        link1Name: "Heroku",
+        link2: false,
+        link2Name: false
+    },
+
+    // {
+    //     name: "",
+    //     lang: ["",""],
+    //     desc: "",
+    //     git: "",
+    //     link1: "",
+    //     link1Name: "",
+    //     link2: "",
+    //     link2Name: ""
+    // },
+]
+
+export default portfolio
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,76 +1,26 @@
 import React, { Component } from 'react';
 
 import Navbar from '../components/Navbar'
+
+import portfolio from "../GitList"
 import PortfolioDisplay from "../components/PortfolioDisplay"
 
-class Portfolio extends Component {
-    state = {
-        portfolio: [
-            {
-                name: "Rock, Paper, Scissors",
-                lang: ["Javascript", "Firebase"],
-                desc: "Multiplayer is enabled through use of google firebase",
-                git: "https://github.com/MaxL999/07-RPS-Multiplayer",
-                link1: "https://maxl999.github.io/07-RPS-Multiplayer/singleplayer.html",
-                link1Name: "Singleplayer",
-                link2: "https://maxl999.github.io/07-RPS-Multiplayer/",
-                link2Name: "Multiplayer"
-            }, {
-                name: "Hangman!",
-                lang: ["Javascript"],
-                desc: "A classic hangman game made with constructors that can be accessed on a webpage or directly thorugh a terminal using Node.js",
-                git: "https://github.com/MaxL999/3-WordGuess",
-                link1: "https://maxl999.github.io/3-WordGuess/",
-                link1Name: "Webpage",
-                link2: "https://github.com/MaxL999/9-hangman.node",
-                link2Name: "Terminal Proof"
-            }, {
-                name: "Minesweeper",
-                lang: ["Javascript"],
-                desc: "The classic minesweeper game",
-                git: "https://github.com/MaxL999/minesweeper",
-                link1: "https://maxl999.github.io/minesweeper/",
-                link1Name: "Minesweeper",
-                link2: false,
-                link2Name: false
-            }, {
-                name: "Burger-Breakin",
-                lang: ["MYSQL", "Handlebars", "Heroku"],
-                desc: "A simple app where you can create and parse data, deployed unto the cloud with Heroku",
-                git: "https://github.com/MaxL999/12-burgerHandlebars",
-                link1: "https://burger-handlebars999.herokuapp.com/",
-                link1Name: "Heroku",
-                link2: false,
-                link2Name: false
-            }
-            // {
-            //     name: "",
-            //     lang: ["",""],
-            //     desc: "",
-            //     git: "",
-            //     link1: "",
-            //     link1Name: "",
-            //     link2: "",
-            //     link2Name: ""
-            // },
-        ]
-    }
-    render() {
-        return (
-            <>
-                <Navbar />
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="card-deck m-0">
-                            {this.state.portfolio.map((project, i) => (
-                                <PortfolioDisplay {...project} className="col-md-4 col-xl-3s" key={i} />
-                            ))}
-                        </div>
+function Portfolio() {
+    return (
+        <>
+            <Navbar />
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="card-deck m-0">
+                        {portfolio.map((project, i) => (
+                            <PortfolioDisplay {...project} className="col-md-4 col-xl-3s" key={i} />
+                        ))}
                     </div>
                 </div>
-            </>
-        )
-    }
+            </div>
+        </>
+    )
+
 }
 
 export default Portfolio
