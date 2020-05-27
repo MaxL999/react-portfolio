ffe321d2724d79d90c0ad6f72662a0179a9031f1
Max Lewis <max.a.lewis98@gmail.com>
2020-05-27 12:27:05 -0700

    more heroku fix attempts

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const path = require("path")
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+var history = require('connect-history-api-fallback');
+app.use(history())
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     // app.use(express.static("client/build"));
     app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
-// // Serve the static files from the React app
-// app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/public/index.html'), function (err) {
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build'), function (err) {
         if (err) res.status(500).send(err)
     })
 })
