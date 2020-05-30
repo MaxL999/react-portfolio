d35babed62837cae0e5f37e257679468d29da819
Max Lewis <max.a.lewis98@gmail.com>
2020-05-29 22:03:54 -0700

    portfolio progress bar

--- client/src/components/PortfolioDisplayProgress.js
+++ client/src/components/PortfolioDisplayProgress.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
-function PortfolioDisplayProgress(props) {
-    let progress = Number(props.progress)
-
-    if (progress > 75) {
-        return <ProgressBar variant="success" now={progress} animated/>;
+function PortfolioDisplayProgress({ progress }) {
+    if (progress === 100) {
+        return <ProgressBar variant="success" now={progress} animated
+            label={<p className="tItalic m-0">Finished</p>} />;
+    } else if (progress > 75) {
+        return <ProgressBar variant="success" now={progress} animated
+            label={<p className="tItalic m-0">Unpolished</p>} />;
     } else if (progress > 50) {
-        return <ProgressBar variant="warning" now={progress} animated/>;
+        return <ProgressBar variant="warning" now={progress} animated
+            label={<p className="tItalic m-0">Partial</p>} />;
     } else {
-        return <ProgressBar variant="danger" now={props.progress} animated/>;
+        return <ProgressBar variant="danger" now={progress} animated
+            label={<p className="tItalic p-0">Unfinished</p>} />;
     }
-
 }
 
 export default PortfolioDisplayProgress
