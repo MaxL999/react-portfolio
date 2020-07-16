d809639ba6ac33137676c7f32ea8e8f853c3fcca
Max Lewis <max.a.lewis98@gmail.com>
2020-07-16 10:19:26 -0700

    changed the color pallette from cool blue greys to grey greys

--- client/src/components/PortfolioDisplay.js
+++ client/src/components/PortfolioDisplay.js
@@ -8,16 +8,16 @@ function PortfolioDisplay(props) {
     const { theme } = useContext(ThemeContext)
 
     return (
-        <div className={"card text-center h-100" + theme.bg3}>
+        <div className="card text-center h-100">
 
-            <div className={"card-header d-flex justify-content-between align-items-center" + theme.bg4}>
+            <div className={"card-header d-flex justify-content-between align-items-center" + theme.bg3}>
                 <p className={"tHeavy m-0 p-0" + theme.txt1}>{props.name}</p>
 
                 <a href={props.git} className="" target="_blank" rel="noopener noreferrer">
                     <p className={"tBold m-0" + theme.txt2}>GIT</p></a>
             </div>
 
-            <div className="card-body">
+            <div className={"card-body"+ theme.bg2}>
 
                 <h6 className="text-muted d-flex justify-content-around mb-3">
                     {props.lang.map((msg, i) => (
@@ -32,7 +32,7 @@ function PortfolioDisplay(props) {
             {/* put updated time or a color indicator for how finished the project is */}
             <PortfolioDisplayProgress progress={props.progress} />
 
-            <div className={"card-footer d-flex justify-content-start" + theme.bg4}>
+            <div className={"card-footer d-flex justify-content-start" + theme.bg3}>
 
                 <div className="d-block">
                     <a href={props.link1} className={"btn m-1" + theme.btn1}
--- client/src/context/ThemeContext.js
+++ client/src/context/ThemeContext.js
@@ -1,31 +1,30 @@
 import React from 'react';
 
 // names for custom color CSS classes inside style.css
-// bg1 is background, bg4 is forground
 export const Themes = {
     light: {
-        bg1: " Bclr2",
-        bg2: " Bclr3",
-        bg3: " Bclr4",
-        bg4: " Bclr5",
+        bg1: " Bclr1",
+        bg2: " Bclr2",
+        bg3: " Bclr3",
+        bg4: " Bclr4",
         btn1: " btn-outline-dark",
         btn2: " ",
         btn3: " ",
-        txt1: " clr10",
-        txt2: " clr8",
+        txt1: " clr3",
+        txt2: " clr2",
         txt3: " clr1",
     },
     dark: {
-        bg1: " Bclr10",
-        bg2: " Bclr8",
-        bg3: " Bclr6",
-        bg4: " Bclr4",
+        bg1: " Bclr6",
+        bg2: " Bclr5",
+        bg3: " Bclr4",
+        bg4: " Bclr1",
         btn1: " btn-outline-light",
         btn2: " ",
         btn3: " ",
         txt1: " clr1",
-        txt2: " clr8",
-        txt3: " clr10",
+        txt2: " clr2",
+        txt3: " clr3",
     }
 };
 
